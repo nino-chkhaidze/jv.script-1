5bd4de43cf2c40ec9fdcacaecb54d9cc43321645
nino-chkhaidze <todrianino28@gmail.com>
2025-01-21 09:24:28 -0800

    add  index.html

---
+++ script.js
@@ -0,0 +1,14 @@
+var userName = 'tiko';
+console.log(userName)
+var age =21;
+console.log(age)
+var is_boy = false;
+console.log(is_boy)
+
+let userName ='nino';
+ userName2 = 'tiko';
+
+console.log(userName);
+console.log(userName2);
+
+
